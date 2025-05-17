682953057f7626043f09e7b0c34a0237075d11ab
Bain Kennedy <jmken093@marriott.com>
2025-05-17 12:29:30 -0700

    Updating keymaps for trouble.

--- lua/bainfu/core/keymaps.lua
+++ lua/bainfu/core/keymaps.lua
@@ -35,13 +35,13 @@ keymap.set("n", "<leader>tf", "<cmd>tabnew %<CR>", { desc = "Open current buffer
 
 -- trouble
 -- Lua
-keymap.set("n", "<leader>xx", function() require("trouble").toggle() end)
-keymap.set("n", "<leader>xw", function() require("trouble").toggle("workspace_diagnostics") end)
-keymap.set("n", "<leader>xd", function() require("trouble").toggle("document_diagnostics") end)
-keymap.set("n", "<leader>xq", function() require("trouble").toggle("quickfix") end)
-keymap.set("n", "<leader>xl", function() require("trouble").toggle("loclist") end)
-keymap.set("n", "gR", function() require("trouble").toggle("lsp_references") end)
-
+-- keymap.set("n", "<leader>xx", function() require("trouble").toggle() end)
+-- keymap.set("n", "<leader>xw", function() require("trouble").toggle("workspace_diagnostics") end)
+-- keymap.set("n", "<leader>xd", function() require("trouble").toggle("document_diagnostics") end)
+-- keymap.set("n", "<leader>xq", function() require("trouble").toggle("quickfix") end)
+-- keymap.set("n", "<leader>xl", function() require("trouble").toggle("loclist") end)
+-- keymap.set("n", "gR", function() require("trouble").toggle("lsp_references") end)
+--
 
 -- zenmode
 keymap.set("n", "<leader>zm", "<cmd>ZenMode<CR>", { desc = "Zen mode" })
--- lua/bainfu/plugins/trouble.lua
+++ lua/bainfu/plugins/trouble.lua
@@ -23,12 +23,12 @@ return {
   local keymap = vim.keymap -- for conciseness
 
   -- Lua
-  keymap.set("n", "<leader>xx", function() require("trouble").toggle() end, { desc = "Trouble Toggle" })
+  -- keymap.set("n", "<leader>xx", function() require("trouble").toggle() end, { desc = "Trouble Toggle" })
   keymap.set("n", "<leader>xw", function() require("trouble").toggle("workspace_diagnostics") end, { desc = "Trouble Toggle Workspace Diagnostics" })
   keymap.set("n", "<leader>xd", function() require("trouble").toggle("document_diagnostics") end, { desc = "Trouble Toggle Document Diagnostics" })
-  keymap.set("n", "<leader>xq", function() require("trouble").toggle("quickfix") end, { desc = "Trouble Toggle Quickfix" })
-  keymap.set("n", "<leader>xl", function() require("trouble").toggle("loclist") end, { desc = "Trouble Toggle Loclist" })
-  keymap.set("n", "gR", function() require("trouble").toggle("lsp_references") end, { desc = "Trouble Toggle LSP References" })
+  -- keymap.set("n", "<leader>xq", function() require("trouble").toggle("quickfix") end, { desc = "Trouble Toggle Quickfix" })
+  -- keymap.set("n", "<leader>xl", function() require("trouble").toggle("loclist") end, { desc = "Trouble Toggle Loclist" })
+  -- keymap.set("n", "gR", function() require("trouble").toggle("lsp_references") end, { desc = "Trouble Toggle LSP References" })
   
  end
 }
